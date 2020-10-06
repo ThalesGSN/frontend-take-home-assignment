bc04247923e3836893aef2c51fb65b33d77da652
Thales Nunes <thalesgsn@gmail.com>
2020-10-06 09:28:59 -0300

    Tear the some test assertions apart to make the test unitary

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, ShallowWrapper } from 'enzyme';
 import App from './App';
 
 describe('App', () => {
-  it('renders', () => {
-    const component = shallow(<App />);
+  let app: ShallowWrapper;
+  beforeEach(() => {
+    app = shallow(<App />);
+  });
 
-    expect(component.exists()).toEqual(true);
-    expect(component.find('Header').exists()).toEqual(true);
-    expect(component.find('SavingGoalContainer').exists()).toEqual(true);
+  it('should have a Header', () => {
+    expect(app.find('Header').exists()).toEqual(true);
+  });
+
+  it('should have a SavingGoalContainer', () => {
+    expect(app.find('SavingGoalContainer').exists()).toEqual(true);
   });
 });
