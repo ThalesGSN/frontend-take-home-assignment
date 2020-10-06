d856cc12b965dfe4774bbcfce8f3b6a9d04afa4d
Thales Nunes <thalesgsn@gmail.com>
2020-10-06 01:42:34 -0300

    Add start animation for page

--- src/components/SavingGoalContainer/SavingGoalContainer.styles.tsx
+++ src/components/SavingGoalContainer/SavingGoalContainer.styles.tsx
@@ -20,8 +20,43 @@ export const Container = styled.main`
     color: var(--primary-color);
     text-align: center;
     font-weight: 400;
+    animation-name: TitleStart;
+    animation-duration: 500ms;
+    animation-iteration-count: 1;
+    animation-timing-function: linear;
     strong {
       font-weight: 600;
+    }
+  }
+
+  section {
+    animation-name: ContainerStart;
+    animation-duration: 500ms;
+    animation-iteration-count: 1;
+    animation-timing-function: linear;
+  }
+
+  @keyframes ContainerStart {
+    from {
+      transform: translateY(30px);
+      opacity: 0;
+    }
+
+    to {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes TitleStart {
+    from {
+      transform: translateY(-10px);
+      opacity: 0;
+    }
+
+    to {
+      transform: translateY(0);
+      opacity: 1;
     }
   }
 `;
